267e9c87863369f0500f7a28c5d013f27c80a8a2
Christopher Rigor <crigor@gmail.com>
2009-02-01 21:16:16 +0800

    move update to model

--- lib/models.rb
+++ lib/models.rb
@@ -7,4 +7,19 @@ class Torrent
   property :added_at, DateTime
   property :created_at, DateTime
   property :updated_at, DateTime
+
+  def self.update
+    urls = %w(http://www.mininova.org/rss.xml?user=axxo http://www.mininova.org/rss.xml?user=klaxxon http://www.mininova.org/rss.xml?user=fxm)
+    urls.each do |url|
+      feed = FeedParser.parse(url)
+      feed.entries.each do |entry|
+        title = entry.title.gsub(/\(\d+S\/\d+L\)/, '').strip
+        torrent = Torrent.first(:title => title)
+        if torrent.nil?
+          new_torrent = Torrent.new :title => title, :link => entry.link, :added_at => entry.updated_time
+          new_torrent.save
+        end
+      end
+    end
+  end
 end
